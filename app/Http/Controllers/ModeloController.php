290008979c25fbe5a8b202bdedb159da6ade0f96
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2021-08-08 00:38:20 +0200

    implementando recurso modelo

--- app/Http/Controllers/ModeloController.php
+++ app/Http/Controllers/ModeloController.php
@@ -4,9 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Models\Modelo;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Storage;
 class ModeloController extends Controller
 {
+    /**
+     * Injecting the Model instance as contructor method .
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function __construct(Modelo $modelo)
+    {
+        $this->modelo = $modelo;
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +24,8 @@ class ModeloController extends Controller
      */
     public function index()
     {
-        //
+        $modelos = $this->modelo->all();
+        return response()->json(['modelos' => $modelos], 200);
     }
 
     /**
@@ -35,18 +46,34 @@ class ModeloController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate($this->modelo->rules(), $this->modelo->feedback());
+        $modelo = $this->modelo;
+        $modelo->marca_id = $request->get('marca_id');
+        $modelo->modelo = $request->get('modelo');
+        $imagem = $request->imagem;
+        $modelo->imagem =  $imagem->store('images/modelos', 'public');
+        $modelo->numero_portas = $request->get('numero_portas');
+        $modelo->lugares = $request->get('lugares');
+        $modelo->air_bag = $request->get('air_bag');
+        $modelo->abs = $request->get('abs');
+        $modelo->save();
+
+        return response()->json(['modelo' => $modelo], 201);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Modelo  $modelo
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function show(Modelo $modelo)
+    public function show($id)
     {
-        //
+        $modelo = $this->modelo->find($id);
+        if ($modelo === null) {
+            return response()->json(["error" => "Not Found"], 404);
+        }
+        return response()->json(['modelo' => $modelo], 200);
     }
 
     /**
@@ -64,22 +91,72 @@ class ModeloController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Modelo  $modelo
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Modelo $modelo)
+    public function update(Request $request, $id)
     {
-        //
+        $modelo = $this->modelo->find($id);
+        if ($modelo === null) {
+            return response()->json(["error" => "Not Found"], 404);
+        }
+        switch ($request->method()) {
+            case 'PUT':
+                $request->validate($modelo->rules(), $modelo->feedback());
+                break;
+            case 'PATCH':
+                $dynamic_rules = [];
+                foreach ($modelo->rules() as $input => $rule) {
+                    if( array_key_exists( $input, $request->all() ) ) {
+                        $dynamic_rules[$input] = $rule;
+                    }
+                }
+                $request->validate($dynamic_rules, $modelo->feedback());
+                break;
+        }
+        if ($request->get('marca_id') != null) {
+            $modelo->marca_id = $request->get('marca_id');
+        }
+        if ($request->get('modelo') != null) {
+            $modelo->modelo = $request->get('modelo');
+        }
+        if ($request->imagem != null) {
+            Storage::disk('public')->delete($modelo->imagem);
+            $imagem = $request->imagem;
+            $modelo->imagem =  $imagem->store('images/modelos', 'public');
+        }
+        if ($request->get('numero_portas') != null) {
+            $modelo->numero_portas = $request->get('numero_portas');
+        }
+        if ($request->get('lugares') != null) {
+            $modelo->lugares = $request->get('lugares');
+        }
+        if ($request->get('air_bag') != null) {
+            $modelo->air_bag = $request->get('air_bag');
+        }
+        if ($request->get('abs') != null) {
+            $modelo->abs = $request->get('abs');
+        }
+        $modelo->update();
+        return response()->json(['modelo' => $modelo], 200);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Modelo  $modelo
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Modelo $modelo)
+    public function destroy($id)
     {
-        //
+        $modelo = $this->modelo->find($id);
+        if ($modelo === null) {
+            return response()->json(["error" => "Not Found"], 404);
+        }
+        Storage::disk('public')->move($modelo->imagem, 'deleted_' . $modelo->imagem);
+        $modelo->imagem = 'deleted_' . $modelo->imagem;
+        $modelo->update();
+        $modelo->delete();
+        return response()->json(['modelo' => $modelo], 200);
     }
 }
--- app/Models/Modelo.php
+++ app/Models/Modelo.php
@@ -10,4 +10,45 @@ class Modelo extends Model
 {
     use HasFactory;
     use SoftDeletes;
+
+    protected $fillable = [
+        'marca_id',
+        'modelo',
+        'imagem',
+        'numero_portas',
+        'lugares',
+        'air_bag',
+        'abs',
+    ];
+
+    public function rules()
+    {
+        return [
+            'marca_id' => 'required|exists:marcas,id',
+            'modelo' => 'required|min:5|max:30|unique:modelos,modelo,' . $this->id,
+            'imagem' => 'required|file|mimes:png',
+            'numero_portas' => 'required|integer|between:2,5',
+            'lugares' => 'required|integer|between:2,20',
+            'air_bag' => 'required|boolean',
+            'abs' => 'required|boolean',
+        ];
+    }
+
+    public function feedback()
+    {
+        return [
+            'required' => 'O campo :attribute não é valido',
+            'unique' => 'O campo :attribute já esta cadastrado',
+            'file' => 'Imagem deve ser um arquivo',
+            'mimes' => 'A extenção do campo :attribute não é valida',
+            'integer' => 'O campo :attribute não é valido',
+            'boolean' => 'O campo :attribute não é valido',
+            'marca.exist' => 'O campo :attribute não é valido',
+            'modelo.min' => 'O campo :attribute deve ter ao menos 5 caracteres',
+            'modelo.max' => 'O campo :attribute deve ter até 30 caracteres',
+            'modelo.unique' => 'O campo :attribute já foi cadastrado',
+            'numero_portas.between' => 'O campo :attribute deve ser entre 2 e 5',
+            'lugares.between' => 'O campo :attribute deve ser entre 2 e 20',
+        ];
+    }
 }
