abecaa0a92d5d4b1bab83d5ffe64fe68a442f1c0
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2021-08-10 17:25:38 +0200

    Obtendo e renovando JWT token

--- routes/api.php
+++ routes/api.php
@@ -25,11 +25,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('login', [AuthController::class, 'login']);
-Route::post('logout', [AuthController::class, 'logout']);
-Route::post('refresh', [AuthController::class, 'refresh']);
-Route::post('me', [AuthController::class, 'me']);
 
 Route::prefix('v1')->middleware('jwt.auth')->group( function() {
+    Route::post('me', [AuthController::class, 'me']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('logout', [AuthController::class, 'logout']);
     Route::apiResource('cliente', ClienteController::class);
     Route::apiResource('locacao', LocacaoController::class);
     Route::apiResource('marca', MarcaController::class);
